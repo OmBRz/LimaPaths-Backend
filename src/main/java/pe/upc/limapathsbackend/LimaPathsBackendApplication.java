49979f23c4ce55716615c786dc97fa6857e13a40
OmBRz <omarbrrcl@gmail.com>
2024-11-04 00:23:50 -0500

    chore: initial commit.

---
+++ src/main/java/pe/upc/limapathsbackend/LimaPathsBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.upc.limapathsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimaPathsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimaPathsBackendApplication.class, args);
+	}
+
+}
